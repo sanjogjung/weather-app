f1e0d3b2006f36c9654b4e07807daa663969639a
Sanjog gc <gcsanjog@gmail.com>
2022-03-23 18:58:51 +0545

    fixed port problem

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const { forecast } = require('./utils/forecast')
 
 const app = express()
 
-const port = process.env.PORT | 3000
+const port = process.env.PORT || 3000
 // paths for express config
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname, '../templates/views')
